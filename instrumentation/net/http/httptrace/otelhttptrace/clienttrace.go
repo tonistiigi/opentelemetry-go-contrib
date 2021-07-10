744c0e0c222db0eec73560bc7f61c52edf28f406
Tonis Tiigi <tonistiigi@gmail.com>
2021-07-09 17:18:24 -0700

    otelhttptrace: get tracer from request if available
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>

--- instrumentation/net/http/httptrace/otelhttptrace/clienttrace.go
+++ instrumentation/net/http/httptrace/otelhttptrace/clienttrace.go
@@ -151,7 +151,14 @@ func NewClientTrace(ctx context.Context, opts ...ClientTraceOption) *httptrace.C
 		opt.apply(ct)
 	}
 
-	ct.tr = otel.GetTracerProvider().Tracer(
+	var tp trace.TracerProvider
+	if span := trace.SpanFromContext(ctx); span.SpanContext().IsValid() {
+		tp = span.TracerProvider()
+	} else {
+		tp = otel.GetTracerProvider()
+	}
+
+	ct.tr = tp.Tracer(
 		"go.opentelemetry.io/otel/instrumentation/httptrace",
 		trace.WithInstrumentationVersion(SemVersion()),
 	)
